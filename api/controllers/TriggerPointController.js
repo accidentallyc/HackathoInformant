84a42669da329b795b7d0443d7b169a8be1c9901
Cassey <cassey@mewe.org>
2016-01-09 14:13:40 +0800

    regex for trigger expression

--- api/controllers/TriggerPointController.js
+++ api/controllers/TriggerPointController.js
@@ -1,13 +1,48 @@
 module.exports = {
    trigger : function(req,res){
+      var id = req.params.id
       Trigger
-         .find({id:req.params.id})
+         .findOne({id:id})
          .then( function(trigger){
-            trigger.expression
-            res.json(trigger)
+            var reqParams = req.params.all()
+            if (!trigger){
+               res.notFound('404: Cannot find trigger with id ' + id )
+            }
+            else{
+               var regex = /\$(\w+)/g
+               var expression = trigger
+                  .expression
+                  .replace( regex, function(match, submatch){
+                     replacement = reqParams[ submatch ] || wordbank[ submatch ]
+                     if (replacement)
+                        return replacement
+                     else
+                        throw new Error('Cannot find match for ' + submatch + ' in both request variables and in the wordbank')
+
+                  })
+
+               var eResult = null
+               var status  = null
+               if( eResult = eval( expression ) ){
+                  msg = new Sms()
+                  msg.content = reqParams.message
+                  msg.send()
+                  
+                  status = 'Passed condition. sending.'
+               } else{
+                  status = 'Failed condition. Not sending.'
+               }
+
+               // Return result
+               res.json({
+                  status : status,
+                  parsed : expression,
+                  result : eResult
+               })
+            }
          })
          .catch( function(err){
-            res.json(trigger)
+            res.json(err)
          })
    }
 }
